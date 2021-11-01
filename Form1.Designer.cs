ee0513e565bc732e3b124f94a7594487b482ff8d
lauendewrau <77196217+lauendewrau@users.noreply.github.com>
2021-11-01 19:04:51 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelMain = new System.Windows.Forms.Panel();
+            this.PanelHead = new System.Windows.Forms.Panel();
+            this.LabelHead = new System.Windows.Forms.Label();
+            this.ButtonClose = new System.Windows.Forms.Button();
+            this.PanelHead.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelMain
+            // 
+            this.PanelMain.BackColor = System.Drawing.Color.Transparent;
+            this.PanelMain.Location = new System.Drawing.Point(0, 36);
+            this.PanelMain.Name = "PanelMain";
+            this.PanelMain.Size = new System.Drawing.Size(450, 213);
+            this.PanelMain.TabIndex = 0;
+            // 
+            // PanelHead
+            // 
+            this.PanelHead.BackColor = System.Drawing.Color.Transparent;
+            this.PanelHead.Controls.Add(this.LabelHead);
+            this.PanelHead.Controls.Add(this.ButtonClose);
+            this.PanelHead.Location = new System.Drawing.Point(0, 0);
+            this.PanelHead.Name = "PanelHead";
+            this.PanelHead.Size = new System.Drawing.Size(450, 36);
+            this.PanelHead.TabIndex = 1;
+            // 
+            // LabelHead
+            // 
+            this.LabelHead.AutoSize = true;
+            this.LabelHead.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(173)))), ((int)(((byte)(185)))));
+            this.LabelHead.Location = new System.Drawing.Point(12, 10);
+            this.LabelHead.Name = "LabelHead";
+            this.LabelHead.Size = new System.Drawing.Size(113, 13);
+            this.LabelHead.TabIndex = 1;
+            this.LabelHead.Text = "Название программы";
+            // 
+            // ButtonClose
+            // 
+            this.ButtonClose.FlatAppearance.BorderSize = 0;
+            this.ButtonClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(46)))), ((int)(((byte)(52)))));
+            this.ButtonClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(52)))), ((int)(((byte)(59)))));
+            this.ButtonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(173)))), ((int)(((byte)(185)))));
+            this.ButtonClose.Location = new System.Drawing.Point(417, 0);
+            this.ButtonClose.Name = "ButtonClose";
+            this.ButtonClose.Size = new System.Drawing.Size(33, 33);
+            this.ButtonClose.TabIndex = 0;
+            this.ButtonClose.TabStop = false;
+            this.ButtonClose.Text = "✖";
+            this.ButtonClose.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 250);
+            this.Controls.Add(this.PanelHead);
+            this.Controls.Add(this.PanelMain);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PanelHead.ResumeLayout(false);
+            this.PanelHead.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelMain;
+        private System.Windows.Forms.Panel PanelHead;
+        private System.Windows.Forms.Button ButtonClose;
+        private System.Windows.Forms.Label LabelHead;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    // Наследуемся от FormShadow
+    public partial class Form1 : FormShadow
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Плавное закрытие программы
+            async void Exit() { for (Opacity = 1; Opacity > .0; Opacity -= .2) await Task.Delay(7); Close(); }
+            ButtonClose.Click += (s, a) => Exit();
+
+            // Красим форму
+            FormPaint(Color.FromArgb(44, 57, 67), Color.FromArgb(35, 44, 55));
+
+            // Позволяем таскать за заголовок Label и Panel
+            new List<Control> { LabelHead, PanelHead }.ForEach(x =>
+            {
+                x.MouseDown += (s, a) =>
+                {
+                    x.Capture = false; Capture = false; Message m = Message.Create(Handle, 0xA1, new IntPtr(2), IntPtr.Zero); base.WndProc(ref m);
+                };
+            });
+        }
+
+        // Красим форму
+        public void FormPaint(Color color1, Color color2)
+        {
+            void OnPaintEventHandler(object s, PaintEventArgs a)
+            {
+                if (ClientRectangle == Rectangle.Empty)
+                    return;
+
+                var lgb = new LinearGradientBrush(ClientRectangle, Color.Empty, Color.Empty, 90);
+                var cblend = new ColorBlend { Colors = new[] { color1, color1, color2, color2 }, Positions = new[] { 0, 0.13f, 0.13f, 1 } };
+
+                lgb.InterpolationColors = cblend;
+                a.Graphics.FillRectangle(lgb, ClientRectangle);
+            }
+
+            Paint -= OnPaintEventHandler;
+            Paint += OnPaintEventHandler;
+
+            Invalidate();
+        }
+
+        async void Form1_Load(object sender, EventArgs e)
+        {
+            // Плавный запуск формы
+            for (Opacity = 0; Opacity < 1; Opacity += .2) await Task.Delay(10);
+        }
+    }
+}
